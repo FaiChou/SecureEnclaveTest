65fb65614f8cd46cdf276fc8ac2232abd5994b19
FaiChou <faichou.zh@gmail.com>
2024-11-13 19:23:37 +0800

    Initial Commit

---
+++ SecureEnclaveTest/SecureEnclaveTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureEnclaveTestApp.swift
+//  SecureEnclaveTest
+//
+//  Created by 周辉 on 2024/11/13.
+//
+
+import SwiftUI
+
+@main
+struct SecureEnclaveTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
